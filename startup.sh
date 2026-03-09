71963d1cbd1a9d2171757501ce1b747290c93621
Ammar Qammaz <ammarkov@gmail.com>
2026-03-09 13:41:22 +0200

    ...

--- startup.sh
+++ startup.sh
@@ -6,7 +6,7 @@ cd "$DIR"
 source venv/bin/activate
 
 python3 babyMonitor.py /dev/video0 8090&
-python3 babyMonitor.py /dev/video1 8093 
+python3 babyMonitor.py /dev/video1 8093&
 
 python3 portal.py --ip 192.168.1.12 -p 8080 -d 8090 -d 8093
 
